509186cf9bef91fa0ea21e1bdb4399a81673bd17
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-09-03 18:30:00 +0200

    initiate a landing page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="shapeBg--1">&nbsp;</div>
+      <div className="shapeBg--2">&nbsp;</div>
+      <div>&nbsp;</div>
+    </div>
+  );
+}
+
+export default App;
