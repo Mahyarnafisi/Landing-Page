21cdbf53706d8ed0322ad54ed601c2f9f54738f2
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-09-27 22:39:10 +0200

    bug fix
    -Badge component
    -Updated Badge styling function to fix random background color issue

--- src/UI/Badge/Badge.tsx
+++ src/UI/Badge/Badge.tsx
@@ -1,9 +1,22 @@
 import Styles from "./Badge.module.css";
-import { useState } from "react";
-
-function Badge({ children }) {
+import { useEffect, useState } from "react";
+type BadgeProps = {
+  children: React.ReactNode;
+};
+function Badge({ children }: BadgeProps) {
   const [bgColor, setBgColor] = useState("");
   const colorList = ["#ffda79", "#c2f4fb", "#cbdbff"];
+
+  /*Function of Random Background Color for badge */
+  useEffect(() => {
+    const ColorSelection = setInterval(() => {
+      setBgColor(colorList[Math.floor(Math.random() * 3)]);
+    }, 6000);
+    return () => {
+      clearInterval(ColorSelection);
+    };
+  }, []);
+  //
 
   return (
     <div style={{ backgroundColor: `${bgColor}` }} className={Styles.badge}>
